e2f39b0c347bcf2b92b2408925a4e216319837d9
Brian Gardner <me@briangardner.tech>
2020-04-06 09:18:39 -0400

    Add user info row content
    * This is the complete code for the Become A Composer blog post

--- app/src/main/java/com/example/composeblogseries/MainActivity.kt
+++ app/src/main/java/com/example/composeblogseries/MainActivity.kt
@@ -5,29 +5,85 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.graphics.Color
+import androidx.ui.layout.LayoutPadding
+import androidx.ui.layout.Row
 import androidx.ui.material.MaterialTheme
+import androidx.ui.text.TextStyle
+import androidx.ui.text.font.FontWeight
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import androidx.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
             }
         }
     }
 }
 
+// region user info row content
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
+fun UserInfoRow(name: String, handle: String, time: String) {
+    Row(
+        modifier = LayoutPadding(8.dp)
+    ) {
+        DisplayName(name)
+        Handle(handle)
+        PostTime(time)
     }
 }
+
+@Composable
+fun DisplayName(name: String) {
+    Text(
+        text = name,
+        modifier = LayoutPadding(0.dp, 0.dp, 8.dp, 0.dp),
+        style = TextStyle(
+            color = Color.Black,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold
+        )
+    )
+}
+
+@Composable
+fun Handle(handle: String) {
+    Text(
+        text = handle,
+        modifier = LayoutPadding(0.dp, 0.dp, 8.dp, 0.dp),
+        style = TextStyle(
+            color = Color.DarkGray,
+            fontSize = 12.sp
+        )
+    )
+}
+
+@Composable
+fun PostTime(time: String) {
+    Text(
+        text = "time",
+        style = TextStyle(
+            color = Color.DarkGray,
+            fontSize = 12.sp
+        )
+    )
+}
+// endregion
+
+// region preview functions
+@Preview
+@Composable
+fun TwitterPreview() {
+    MaterialTheme {
+        UserInfoRow(
+            name = "Brian Gardner",
+            handle = "@BrianGardnerDev",
+            time = "7m"
+        )
+    }
+}
+// endregion
