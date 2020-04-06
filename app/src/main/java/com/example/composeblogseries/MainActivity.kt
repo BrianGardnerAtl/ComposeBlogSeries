2e42e3e66799455d98f6203cb29cc430633de484
Brian Gardner <me@briangardner.tech>
2020-04-06 09:28:15 -0400

    Add action row and tweet content
    * This completes the second blog post contents

--- app/src/main/java/com/example/composeblogseries/MainActivity.kt
+++ app/src/main/java/com/example/composeblogseries/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.graphics.Color
-import androidx.ui.layout.LayoutPadding
-import androidx.ui.layout.Row
+import androidx.ui.graphics.vector.drawVector
+import androidx.ui.layout.*
+import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
+import androidx.ui.res.vectorResource
 import androidx.ui.text.TextStyle
 import androidx.ui.text.font.FontWeight
 import androidx.ui.tooling.preview.Preview
@@ -23,6 +25,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+}
+
+@Composable
+fun TweetView() {
+    Column {
+        UserInfoRow(
+            name = "Brian Gardner",
+            handle = "@BrianGardnerDev",
+            time = "7m"
+        )
+        TweetContent(content = "Sample tweet content")
+        ActionRow()
+    }
+}
+
+@Composable
+fun TweetContent(content: String) {
+    return Text(
+        text = content,
+        style = TextStyle(
+            color = Color.Black,
+            fontSize = 12.sp
+        ),
+        modifier = LayoutPadding(8.dp)
+    )
 }
 
 // region user info row content
@@ -74,16 +101,76 @@ fun PostTime(time: String) {
 }
 // endregion
 
+// region action row contents
+@Composable
+fun ActionRow() {
+    Row(
+        modifier = LayoutWidth.Fill + LayoutPadding(8.dp),
+        arrangement = Arrangement.SpaceAround
+    ){
+        Comment()
+        Retweet()
+        Like()
+        Share()
+    }
+}
+
+@Composable
+fun Comment() {
+    val icon = vectorResource(R.drawable.ic_comment)
+    Container(
+            height = 24.dp,
+            width = 24.dp,
+            modifier = drawVector(vectorImage = icon, tintColor = Color.LightGray)
+    ) {
+
+    }
+}
+
+@Composable
+fun Retweet() {
+    val icon = vectorResource(R.drawable.ic_retweet)
+    Container(
+            height = 24.dp,
+            width = 24.dp,
+            modifier = drawVector(vectorImage = icon, tintColor = Color.LightGray)
+    ) {
+
+    }
+}
+
+@Composable
+fun Like() {
+    val icon = vectorResource(R.drawable.ic_like)
+    Container(
+            height = 24.dp,
+            width = 24.dp,
+            modifier = drawVector(vectorImage = icon, tintColor = Color.LightGray)
+    ) {
+
+    }
+}
+
+@Composable
+fun Share() {
+    val icon = vectorResource(R.drawable.ic_share)
+    Container(
+            height = 24.dp,
+            width = 24.dp,
+            modifier = drawVector(vectorImage = icon, tintColor = Color.LightGray)
+    ) {
+
+    }
+}
+// endregion
+
 // region preview functions
 @Preview
 @Composable
 fun TwitterPreview() {
     MaterialTheme {
-        UserInfoRow(
-            name = "Brian Gardner",
-            handle = "@BrianGardnerDev",
-            time = "7m"
-        )
+        TweetView()
+
     }
 }
 // endregion
